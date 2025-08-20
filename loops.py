c39f9adbe1db5ae6e333f2eeed5ed749f5fbe2c3
João Paulo <alvessantos0710@gmail.com>
2025-08-20 18:20:53 -0300

    Aula 3

---
+++ loops.py
@@ -0,0 +1,13 @@
+#contador = 0
+numero1 = int(input(input("digite um inteiro"))
+numero2 = int(input('digite outro inteiro'))
+
+while numero != numero2 :
+
+if numero1 = numero2:
+   numero > numero2
+
+else: 
+      numero2 = nuemro - numero1
+
+      print('numero iguais')
---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+numero_aleatorio = random.randint(1,10)
+palpite_jogador = int(input('digite um numeiro inteiro de 1 a 10'))
+numero_tentativas = 1
+
+while numero_aleatorio != palpite_jogador:
+    numero_tentativas += 1
+
+    if palpite_jogador > numero_aleatorio:
+        print('muito alto')
+    else:
+        print('muito baixo')
+
+    palpite_jogador = int(input('digite um inteiro de 1 a 10'))
+    
+    print(f"acertou em {numero_tentativas} tentativas")
